b241c798035aa5334f4d97bdc5c82a09d76a580c
Blair Williams <blair.l.williams@intel.com>
2021-05-18 15:29:57 -0700

    Updates to Client API

--- lib/workflowmax-ruby/api/client_api.rb
+++ lib/workflowmax-ruby/api/client_api.rb
@@ -6,6 +6,134 @@ module WorkflowMaxRuby
       @api_client = api_client
     end
 
+    # Retrieves the full chart of accounts
+    # @param xero_tenant_id [String] Xero identifier for Tenant
+    # @param [Hash] opts the optional parameters
+    # @option opts [DateTime] :if_modified_since Only records created or modified since this timestamp will be returned
+    # @option opts [String] :where Filter by an any element
+    # @option opts [String] :order Order by an any element
+    # @return [Clients]
+    def get_clients(xero_tenant_id, opts = {})
+      data, _status_code, _headers = get_clients_with_http_info(xero_tenant_id, opts)
+      data
+    end
+
+    # Retrieves the full chart of accounts
+    # @param xero_tenant_id [String] Xero identifier for Tenant
+    # @param [Hash] opts the optional parameters
+    # @option opts [DateTime] :if_modified_since Only records created or modified since this timestamp will be returned
+    # @option opts [String] :where Filter by an any element
+    # @option opts [String] :order Order by an any element
+    # @return [Array<(Accounts, Integer, Hash)>] Accounts data, response status code and response headers
+    def get_clients_with_http_info(xero_tenant_id, options = {})
+      opts = options.dup
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: ClientApi.get_clients ...'
+      end
+      # verify the required parameter 'xero_tenant_id' is set
+      if @api_client.config.client_side_validation && xero_tenant_id.nil?
+        fail ArgumentError, "Missing the required parameter 'xero_tenant_id' when calling ClientApi.get_clients"
+      end
+      # resource path
+      local_var_path = '/Clients'
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      header_params[:'xero-tenant-id'] = xero_tenant_id
+      header_params[:'If-Modified-Since'] = opts[:'if_modified_since'] if !opts[:'if_modified_since'].nil?
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:body]
+
+      # return_type
+      return_type = opts[:return_type] || 'Clients'
+
+      # auth_names
+      auth_names = opts[:auth_names] || ['OAuth2']
+
+      new_options = opts.merge(
+        :header_params => header_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, "ClientApi", new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: ClientAPI#get_clients\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
+    # Retrieves a single chart of accounts by using a unique account Id
+    # @param xero_tenant_id [String] Xero identifier for Tenant
+    # @param client_id [String] Unique identifier for Account object
+    # @param [Hash] opts the optional parameters
+    # @return [Clients]
+    def get_client(xero_tenant_id, client_id, opts = {})
+      data, _status_code, _headers = get_client_with_http_info(xero_tenant_id, client_id, opts)
+      data
+    end
+
+    # Retrieves a single chart of accounts by using a unique account Id
+    # @param xero_tenant_id [String] Xero identifier for Tenant
+    # @param client_id [String] Unique identifier for Account object
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(Clients, Integer, Hash)>] Clients data, response status code and response headers
+    def get_client_with_http_info(xero_tenant_id, client_id, options = {})
+      opts = options.dup
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: ClientApi.get_client ...'
+      end
+      # verify the required parameter 'xero_tenant_id' is set
+      if @api_client.config.client_side_validation && xero_tenant_id.nil?
+        fail ArgumentError, "Missing the required parameter 'xero_tenant_id' when calling ClientApi.get_client"
+      end
+      # verify the required parameter 'account_id' is set
+      if @api_client.config.client_side_validation && client_id.nil?
+        fail ArgumentError, "Missing the required parameter 'account_id' when calling ClientApi.get_client"
+      end
+      # resource path
+      local_var_path = '/Clients/{ClientID}'.sub('{' + 'ClientID' + '}', client_id.to_s)
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      header_params[:'xero-tenant-id'] = xero_tenant_id
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:body]
+
+      # return_type
+      return_type = opts[:return_type] || 'Clients'
+
+      # auth_names
+      auth_names = opts[:auth_names] || ['OAuth2']
+
+      new_options = opts.merge(
+        :header_params => header_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type
+      )
+
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path, "ClientAPI", new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: ClientApi#get_account\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
   end
 end
 
