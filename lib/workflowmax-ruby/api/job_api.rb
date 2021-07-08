cb9be00b142ced980237bcbe71c2f60a188ffbc8
Blair Williams <blair.l.williams@intel.com>
2021-07-07 19:16:42 -0700

    Updates for jobs for tasks and milestones

--- lib/workflowmax-ruby/api/job_api.rb
+++ lib/workflowmax-ruby/api/job_api.rb
@@ -35,7 +35,7 @@ module WorkflowMaxRuby
         fail ArgumentError, "Missing the required parameter 'xero_tenant_id' when calling JobApi.get_jobs"
       end
       # resource path
-      local_var_path = 'current'
+      local_var_path = 'current?detailed=true'
 
       # header parameters
       header_params = opts[:header_params] || {}
