220ad9cd38950a3b3651dc47aed4cd78255a9529
Blair Williams <blair.l.williams@intel.com>
2021-10-12 14:52:19 -0700

    Updates for changes to faraday

--- lib/workflowmax-ruby/api_client.rb
+++ lib/workflowmax-ruby/api_client.rb
@@ -296,7 +296,7 @@ module WorkflowMaxRuby
       end
 
       connection = Faraday.new(:url => method_base_url, :ssl => ssl_options) do |conn|
-        conn.basic_auth(config.username, config.password)
+        conn.request :authorization, :basic, config.username, config.password
         if opts[:header_params]["Content-Type"] == "multipart/form-data"
           conn.request :multipart
           conn.request :url_encoded
